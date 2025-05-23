51be99737f85e22524d0852989345f777c095a8e
bluetrap <1733607992@qq.com>
2025-05-23 16:08:16 +0800

    feat:创建子包及项目

---
+++ apps/react-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Input } from "@blu3trap/ui";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/ui/src/components/Input/Input.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Input() {
+  return (
+    <div>
+      测试组件12313：
+      <input type="text" placeholder="Basic Input" />
+    </div>
+  );
+}
